584f496fc101026a0c17ca9ba48b0ecd805f71ce
Christian Sophocleous <c-sophocleous@hotmail.com>
2022-03-02 14:41:32 +0000

    initial commit

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -2,6 +2,8 @@ function PageHeader(): JSX.Element {
   return (
     <header>
       <h1>Ye Olde Web Page</h1>
+      <h1>I am Batman</h1>
+      <h1>Do you feel in charge</h1>
       <p>Welcome to Nichard's Ye Olde Web Page!</p>
       <p>
         This shows a random scatter of my thoughts, last updated on March 4,
